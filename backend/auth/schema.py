a66c3b3452a852de36436166e784d208e5d374ab
Rudy Savini <rudy.savini@fitstic-edu.com>
2025-02-05 14:02:17 +0100

    feat: implement registration
    Author: Rudy Savini - @SavGRY

---
+++ backend/auth/schema.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    first_name: str | None
+    last_name: str | None
+    email: str
+    is_superuser: bool
+    is_active: bool
+    full_name: str | None
+
+    class Config:
+        orm_mode = True
+
+
+class UserFromForm(BaseModel):
+    email: str
+    password: str
+
+
+class UserInDB(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    hashed_password: str
+    is_superuser: bool
+    is_active: bool
+    full_name: str
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str
--- backend/core/main.py
+++ backend/core/main.py
@@ -1,14 +1,38 @@
+from auth.schema import User as UserSchema
+from auth.schema import UserFromForm
+from auth.services import (
+    check_user_already_registered,
+    get_password_hash,
+    validate_email,
+)
 from core.db.database import engine, get_db
-from core.db.models import Base
-from fastapi import FastAPI
+from core.db.models import Base, User
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-db = get_db()
-
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
+
+
+@app.post("/register", response_model=UserSchema)
+async def register_user(payload: UserFromForm, db: Session = Depends(get_db)):
+    validate_email(payload.email)
+    check_user_already_registered(payload.email)
+
+    hashed_password = get_password_hash(password=payload.password)
+    new_user = User(
+        email=payload.email,
+        password=hashed_password,
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
