1cb8f58cc2f26a984b8d5405415878ed1eeb1560
Rudy Savini <savingru@libero.it>
2025-02-16 21:11:00 +0100

    feat(web): add login api call
    - Added new service to call the `/login` endpoint, with some basic
      error handling
    - Refined some styling options
    
    Author: Rudy Savini - @SavGRY

---
+++ frontend/incassi/src/app/services/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Injectable, inject} from '@angular/core'
+import type {Observable} from 'rxjs'
+import type {LoginResponse} from './Models'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly http: HttpClient = inject(HttpClient)
+  private readonly API_URL: string = 'http://localhost:8000'
+  private readonly httpOptions: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded',
+  })
+
+  login(email: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.API_URL}/login`, `email=${email}&password=${password}`, {
+      headers: this.httpOptions,
+      withCredentials: true,
+    })
+  }
+}
