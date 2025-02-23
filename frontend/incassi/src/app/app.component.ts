8b6772e8590b78e9d624fcdb49c37c44a591a2b6
Rudy Savini <rudy.savini@fitstic-edu.com>
2025-02-23 13:03:26 +0100

    feat(frontend-ui): create first dark mode switcher
    Author: Rudy Savini - @SavGRY

--- frontend/incassi/src/app/app.component.ts
+++ frontend/incassi/src/app/app.component.ts
@@ -1,12 +1,48 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
+import {initFlowbite} from 'flowbite'
+import {BottomNavComponent} from './bottom-nav/bottom-nav.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BottomNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'incassi'
+
+  ngOnInit(): void {
+    initFlowbite()
+    this.updateTheme(this.isDarkMode())
+  }
+
+  private isDarkMode(): boolean {
+    return (
+      localStorage.getItem('color-theme') === 'dark' ||
+      (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    )
+  }
+
+  private updateTheme(isDark: boolean): void {
+    const darkIcon = document.getElementById('theme-toggle-dark-icon')
+    const lightIcon = document.getElementById('theme-toggle-light-icon')
+
+    if (isDark) {
+      document.documentElement.classList.add('dark')
+      lightIcon?.classList.remove('hidden')
+      darkIcon?.classList.add('hidden')
+    } else {
+      document.documentElement.classList.remove('dark')
+      darkIcon?.classList.remove('hidden')
+      lightIcon?.classList.add('hidden')
+    }
+
+    localStorage.setItem('color-theme', isDark ? 'dark' : 'light')
+  }
+
+  switchMode(): void {
+    const currentMode = this.isDarkMode()
+    this.updateTheme(!currentMode)
+  }
 }
