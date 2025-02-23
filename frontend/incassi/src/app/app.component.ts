db4896399ff389076494b41a2f35fbd044bbfafd
Rudy Savini <rudy.savini@fitstic-edu.com>
2025-02-23 22:55:02 +0100

    feat(frontend-ui): create bottom nav
    Author: Rudy Savini - @SavGRY

--- frontend/incassi/src/app/app.component.ts
+++ frontend/incassi/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core'
-import {RouterOutlet} from '@angular/router'
-import {initFlowbite} from 'flowbite'
-import {BottomNavComponent} from './bottom-nav/bottom-nav.component'
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {initFlowbite} from 'flowbite';
+import {BottomNavComponent} from './bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-root',
@@ -10,39 +10,9 @@ import {BottomNavComponent} from './bottom-nav/bottom-nav.component'
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'incassi'
+  title = 'incassi';
 
   ngOnInit(): void {
-    initFlowbite()
-    this.updateTheme(this.isDarkMode())
-  }
-
-  private isDarkMode(): boolean {
-    return (
-      localStorage.getItem('color-theme') === 'dark' ||
-      (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-    )
-  }
-
-  private updateTheme(isDark: boolean): void {
-    const darkIcon = document.getElementById('theme-toggle-dark-icon')
-    const lightIcon = document.getElementById('theme-toggle-light-icon')
-
-    if (isDark) {
-      document.documentElement.classList.add('dark')
-      lightIcon?.classList.remove('hidden')
-      darkIcon?.classList.add('hidden')
-    } else {
-      document.documentElement.classList.remove('dark')
-      darkIcon?.classList.remove('hidden')
-      lightIcon?.classList.add('hidden')
-    }
-
-    localStorage.setItem('color-theme', isDark ? 'dark' : 'light')
-  }
-
-  switchMode(): void {
-    const currentMode = this.isDarkMode()
-    this.updateTheme(!currentMode)
+    initFlowbite();
   }
 }
--- frontend/incassi/src/app/services/auth/auth.service.ts
+++ frontend/incassi/src/app/services/auth/auth.service.ts
@@ -1,22 +1,22 @@
-import {HttpClient, HttpHeaders} from '@angular/common/http'
-import {Injectable, inject, signal} from '@angular/core'
-import type {Observable} from 'rxjs'
-import type {LoginResponse} from './Models'
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable, inject, signal} from '@angular/core';
+import type {Observable} from 'rxjs';
+import type {LoginResponse} from './Models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly http: HttpClient = inject(HttpClient)
-  private readonly API_URL: string = 'http://localhost:8000/api/v1/auth'
+  private readonly http: HttpClient = inject(HttpClient);
+  private readonly API_URL: string = 'http://localhost:8000/api/v1/auth';
   private readonly httpOptions: HttpHeaders = new HttpHeaders({
     'Content-Type': 'application/x-www-form-urlencoded',
-  })
-  token = signal('')
+  });
+  token = signal('');
   login(email: string, password: string): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.API_URL}/login`, `email=${email}&password=${password}`, {
       headers: this.httpOptions,
       withCredentials: true,
-    })
+    });
   }
 }
