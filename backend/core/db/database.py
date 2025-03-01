29146f46dc95b9235cf1f2abbe41d5b282dea909
Rudy Savini <rudy.savini@fitstic-edu.com>
2025-03-01 21:10:07 +0100

    fix: remove psql hadrcoded str connection
    Author: Rudy Savini - @SavGRY

--- backend/core/db/database.py
+++ backend/core/db/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db/db"
+SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRES_CONNECTION_STR", None)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
