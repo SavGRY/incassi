0a78652b4b726e8aadb3dde90ca5c63493c425c4
Rudy Savini <savingru@libero.it>
2026-01-18 19:12:11 +0100

    refactor(frontend): deprecated old app

--- frontend/incassi/src/app/app.config.ts
+++ frontend/incassi/src/app/app.config.ts
@@ -1,16 +1,16 @@
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {providePrimeNG} from 'primeng/config';
 import {CustomTheme} from '../CustomTheme';
 import {routes} from './app.routes';
+import {authInterceptor} from './services/interceptor/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    // provideHttpClient(withInterceptors([authInterceptor])),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     providePrimeNG({
       theme: {
         preset: CustomTheme,
