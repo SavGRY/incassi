621012c8a2e809355ca76905764c647bb65d8b3f
Rudy Savini <rudy.savini@pytech.it>
2026-03-08 16:10:01 +0100

    refacor(frontend): scss theming
    - Biome schema not matching cli version
    - Modularized scss layer and utilities
    - refactored switch to have less complexity

--- frontend/incassi/src/app/app.spec.ts
+++ frontend/incassi/src/app/app.spec.ts
@@ -41,6 +41,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('app works!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Ciao tuo nome');
   });
 });
