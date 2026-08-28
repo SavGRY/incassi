8ad475bf11f24e4ff2c38e82e7827a4a453be072
Rudy Savini <rudy.savini@pytech.it>
2026-08-28 21:52:12 +0200

    deps: migrate primeng openng

--- frontend/incassi/src/app/app.config.ts
+++ frontend/incassi/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideRouter} from '@angular/router';
-import {providePrimeNG} from 'primeng/config';
+import {provideOptimus} from '@openng/optimus-ui/config';
 import {CustomTheme} from '../CustomTheme';
 import {routes} from './app.routes';
 import {authInterceptor} from './services/interceptor/auth-interceptor';
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    providePrimeNG({
+    provideOptimus({
       theme: {
         preset: CustomTheme,
         options: {
