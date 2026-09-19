a11930c922642c9b7770d028260bfbd4e2192c3e
Rudy Savini <rudy.savini@pytech.it>
2026-09-19 18:51:42 +0200

    feat: add minimal gallery file picker
    - No API call for the moment

--- frontend/incassi/src/app/pages/home/home.ts
+++ frontend/incassi/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import {Component, inject, signal} from '@angular/core';
 import {Drawer} from '@openng/optimus-ui/drawer';
+import type {NuovoIncasso} from '../../models/incasso';
 import {Documenti} from '../../services/documenti';
+import {Incassi} from '../../services/incassi';
 import type {NuovoCliente} from '../../shared/cliente-form/cliente-form';
 import {ClienteForm} from '../../shared/cliente-form/cliente-form';
 import {CreateToggler} from '../../shared/create-toggler/create-toggler';
-import type {NuovoIncasso} from '../../shared/incasso-form/incasso-form';
 import {IncassoForm} from '../../shared/incasso-form/incasso-form';
 import {RecentDocuments} from '../../shared/recent-documents/recent-documents';
 
@@ -15,13 +16,14 @@ import {RecentDocuments} from '../../shared/recent-documents/recent-documents';
 })
 export class Home {
   private readonly documentiService = inject(Documenti);
+  private readonly incassiService = inject(Incassi);
   documenti = this.documentiService.documenti;
 
   incassoDrawerAperto = signal(false);
   clienteDrawerAperto = signal(false);
 
   onSalvaIncasso(incasso: NuovoIncasso): void {
-    console.log('Nuovo incasso', incasso);
+    this.incassiService.crea(incasso).subscribe();
     this.incassoDrawerAperto.set(false);
   }
 
