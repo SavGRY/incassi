4235b26735bab06c36885cf15f827c32bcd4fe4c
Rudy Savini <rudy.savini@pytech.it>
2026-09-19 14:32:38 +0200

    feat(frontend): add minimal home

--- frontend/incassi/src/app/app.routes.ts
+++ frontend/incassi/src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import {Routes} from '@angular/router';
 import {Login} from './auth/login/login';
+import {DocumentiList} from './pages/documenti-list/documenti-list';
+import {Home} from './pages/home/home';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'documenti',
+    component: DocumentiList,
+  },
   {
     path: 'login',
     component: Login,
